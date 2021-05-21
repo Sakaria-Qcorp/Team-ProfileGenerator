34a98357094a9839461396f02878f95c9a32d3e4
Zack <ahmedsakaria32@gmail.com>
2021-05-21 13:28:09 -0500

    adding requirements for the main index.js

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
+
+const HTMLrenderring = require("./lib/htmlRenderer");
 const fs = require("fs");
 
