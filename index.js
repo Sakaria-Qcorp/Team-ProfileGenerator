7791560b46524d4de5af2a9758be9bd92ae17008
Zack <ahmedsakaria32@gmail.com>
2021-05-21 14:48:10 -0500

    adding prompts to the index.js

--- index.js
+++ index.js
@@ -6,3 +6,38 @@ const inquirer = require("inquirer");
 const HTMLrenderring = require("./lib/htmlRenderer");
 const fs = require("fs");
 
+const teamArrayResults= [];
+
+async function getManagerInfo(){
+    try{
+    let response= await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Name of the manager.: '
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Enter the manager ID here: '
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is email for manager? : ',
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: 'What is the office number for the employee?: '
+        }]);
+        const manager= new Manager(response.name, response.id, response.email, response.officeNumber);
+        teamArrayResults.push(manager);
+    }catch(err){
+        return console.log(err);
+        }
+        console.log(teamArray);
+   question();     
+};
+
+getManagerInfo();
