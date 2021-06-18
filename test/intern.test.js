68d0d04d4514842f53e94d47f6c2cc13f3833f61
Zack <ahmedsakaria32@gmail.com>
2021-06-18 12:13:38 -0500

    adding a test for intern

---
+++ test/intern.test.js
@@ -0,0 +1,25 @@
+const Intern = require('../lib/Intern');
+
+describe("getSchool", () => {
+
+    it("should console log the school", () => {
+
+        const school = "University of Minnesota - TC"
+        const intern = new Intern("", "", "", school);
+
+        expect(intern.getSchool()).toBe(school);
+    })
+
+});
+
+describe("getRole", () => {
+
+    it("should console log the role", () => {
+
+        const role = "Intern"
+        const intern = new Intern("", "", "", "")
+
+        expect(intern.getRole()).toBe(role);
+    })
+
+});
